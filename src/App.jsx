1a6ed2b9faa9c74178356d11a6254ae8c54d05f5
Luis Calderon <luis.calderon.lc72353@gmail.com>
2021-07-24 21:14:28 -0600

    fetch data and show compatible crops

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-
+import data from "./data.json";
 
 function App() {
+
+  let inputCrop="";
+
+  function fetchCrop(e){
+    e.preventDefault();
+    const suggestion=data.crops[inputCrop];
+    if(suggestion){
+      console.log(inputCrop.toUpperCase());
+      console.log(JSON.stringify(suggestion,null,"\t"));
+    }else{
+      console.log(`no se encontraron datos para "${inputCrop}"`);
+    }
+  }
+
+  function inputHandle(e){
+    inputCrop=e.target.value;
+  }
+  
   return (
     <div>
-      Hola Mundo
+      <form  onSubmit={fetchCrop}>
+        <input onInput={inputHandle} type="text" placeholder="nombre de cultivo"/>
+        <button type="submit">fetch data</button>
+      </form>
     </div>
   );
 }
